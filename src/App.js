7fc1680c690bb42778f8116f2aa0e920b30d89d7
jacquelinesz <zuritajackie@gmail.com>
2022-07-27 17:10:50 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Jacqueline Solorzano and is open-sourced on
-        <a
-          href="https://github.com/jacquelinesz/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Jacqueline Solorzano and is open-sourced on
+          <a
+            href="https://github.com/jacquelinesz/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

646082714496d3861124fdc3471a7a6dde27dee8
jacquelinesz <zuritajackie@gmail.com>
2022-07-27 16:59:53 -0400

    Added GitHub repo url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,20 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Jacqueline Solorzano and is open-sourced on
+        <a
+          href="https://github.com/jacquelinesz/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }

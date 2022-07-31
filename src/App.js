a2650dbf47b51d2760c7383ffe3a390223db75c5
jacquelinesz <zuritajackie@gmail.com>
2022-07-31 17:26:49 -0400

    Minor footer edit

--- src/App.js
+++ src/App.js
@@ -8,14 +8,31 @@ export default function App() {
       <div className="container">
         <Weather defaultCity="Charlotte" />
         <footer>
-          This project was coded by Jacqueline Solorzano and is open-sourced on{" "}
+          This project was coded by{" "}
+          <a
+            href="https://transcendent-snickerdoodle-662c96.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Jacqueline Solorzano
+          </a>{" "}
+          and is open-sourced on{" "}
           <a
             href="https://github.com/jacquelinesz/react-weather-app"
             target="_blank"
             rel="noreferrer"
           >
             GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://luxury-licorice-8af185.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
           </a>
+          .
         </footer>
       </div>
     </div>

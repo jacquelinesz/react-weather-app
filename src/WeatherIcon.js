ac1bd49e52fdd15281b009cf43042598bff9ade1
jacquelinesz <zuritajackie@gmail.com>
2022-08-03 16:41:09 -0400

    Created the layout for the first day of the weather forecast

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactAnimatedWeather from "react-animated-weather/build/ReactAnimatedWeather";
+import Weather from "./Weather";
 
 export default function WeatherIcon(props) {
   const codeMapping = {
@@ -26,7 +27,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="black"
-      size={64}
+      size={props.size}
       animate={true}
     />
   );
